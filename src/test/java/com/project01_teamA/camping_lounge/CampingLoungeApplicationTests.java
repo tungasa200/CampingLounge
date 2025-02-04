38ece4c888d3df94e7f790b11a63c9526f5e16bf
20250102 <ksoung140w@gmail.com>
2025-02-04 15:02:46 +0900

    first commit

---
+++ src/test/java/com/project01_teamA/camping_lounge/CampingLoungeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project01_teamA.camping_lounge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingLoungeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
